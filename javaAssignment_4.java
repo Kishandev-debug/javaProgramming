2ff62e448246157e3a8508ffb00ce06dafd0c317
kishandev <115924023+Kishandev-debug@users.noreply.github.com>
2024-11-16 11:33:06 +0530

    create method

--- javaAssignment_4.java
+++ javaAssignment_4.java
@@ -1,21 +1,25 @@
-// Find Factorial for n term and also calculate sum of the factrioal term 
-// Question no-01
 import java.util.Scanner;
+
 public class javaAssignment_4 {
-   public static void main(String args[]){
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter number to find factroial");
-int n=sc.nextInt();
-int factroial=1;
-for(int i=n;i>=1;i--){
-factroial*=i;
-}
-System.out.println("factroial of the "+ n+" is "+factroial);
-String str=Integer.toString(factroial);
-int sum=0;
-for(int i=0;i<str.length();i++){
-    sum += Character.getNumericValue(str.charAt(i));
-}
-System.out.println("Sum is : "+sum);
-   } 
+   public static int sumVar(int val) {
+      String str = Integer.toString(val);
+      int sum = 0;
+      for (int i = 0; i < str.length(); i++) {
+         sum += Character.getNumericValue(str.charAt(i));
+      }
+      return sum;
+   }
+
+   public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter number to find factroial");
+      int n = sc.nextInt();
+      int factroial = 1;
+      for (int i = n; i >= 1; i--) {
+         factroial *= i;
+      }
+      System.out.println("factroial of the " + n + " is " + factroial);
+
+      System.out.println("Sum is : " + sumVar(factroial));
+   }
 }
